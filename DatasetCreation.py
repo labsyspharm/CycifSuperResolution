62dac9d1a6ed719547a837e8a31d8b72d66d350f
Alex Barbera <alex_barbera.m@hotmail.com>
2023-08-07 14:57:25 -0400

    first commit

---
+++ DatasetCreation.py
@@ -0,0 +1,114 @@
+import sklearn
+import tifffile
+import argparse
+import sys
+import os
+import pickle
+import numpy
+import tqdm
+
+def parse_args():
+    output = argparse.ArgumentParser()
+
+    output.add_argument("input", type=str, help="Path to directories with image")
+    output.add_argument("output", type=str, help="Path to store results")
+    output.add_argument("--recursive", action="store_true", help="Iterate through directories.")
+    output.add_argument("--file-suffix", type=str, help="Only select files with this sufix. For testing is \"stack.tif\"", default=None)
+    output.add_argument("--norm-type", choices=["std", "gauss"], help="How to normalize images.\nStd is bit normalization.\nGauss scales the image so the mean is `80% of bit range and then bit normalizes.", default="std")
+    output.add_argument("--prefix", type=str, help="Prefix for generated files", default="")
+    output.add_argument("--transformations", action="store_true", help="Do transformations to images")
+
+    output = output.parse_args(sys.argv[1:])
+
+    if not os.path.isdir(output.input):
+        print("Path {} is not directory.".format(output.input))
+
+    if not os.path.exists(output.output):
+        os.makedirs(output.output, exist_ok=True)
+    if not os.path.exists(os.path.join(output.output, "input")):
+        os.makedirs(os.path.join(output.output, "input"), exist_ok=True)
+    if not os.path.exists(os.path.join(output.output, "output")):
+        os.makedirs(os.path.join(output.output, "output"), exist_ok=True)
+
+    return output
+
+
+def save_combination(args, input_image, output_image, name):
+    with open(os.path.join(args.output, "input", name), "wb") as f:
+        pickle.dump(input_image, f)
+
+    with open(os.path.join(args.output, "output", name), "wb") as f:
+        pickle.dump(output_image, f)
+
+
+def normalize_8_bit(args, image):
+    if image.dtype == numpy.int8 or image.dtype == numpy.uint8:
+        return image / (2**8)  # tecnically not necessary but for completion-wise
+    elif image.dtype == numpy.float16 or image.dtype == numpy.uint16:
+        return image / (2**16)
+    elif image.dtype == numpy.float32 or image.dtype == numpy.uint32:
+        return image / (2**32)
+    elif image.dtype == numpy.float64 or image.dtype == numpy.uint64:
+        return image / (2**64)
+    else:
+        raise Exception("Invalid dtype {}".format(image.dtype))
+
+
+def normalize(args, image):
+    if args.norm_type == "std":
+        return normalize_8_bit(args, image) * 255.0
+    elif args.norm_type == "gauss":
+        """
+        3 component normally separates background noise from nuclei so normalize the top mean to something high
+        and then clip over values
+        """
+        gmm = sklearn.mixture.GaussianMixture(3, max_iter=1000, tol=1e-6)
+        gmm.fit(numpy.log10(image.reshape((-1, 1))))
+        mean = max(10 ** gmm.means_[:, 0])
+        bits = 8 * image.itemsize
+        dtype = image.dtype  # it is important to mantain dtype for normalization later
+
+        tif = (0.8 * 2 ** bits) * image / mean  # 80% of 16 bits is ~52000
+        tif[tif > 2 ** bits] = 2 ** bits
+        return normalize_8_bit(tif.astype(dtype)) * 255.0
+
+
+def handle_file(args, file_path):
+    tiff = tifffile.imread(file_path)
+
+    tiff = normalize(args, tiff)
+
+    input_image = tiff[0]
+
+    for i in range(1, len(tiff)):
+        save_combination(args, input_image, tiff[i], args.prefix + file_path.split("/")[-1] + "_" + str(i))
+
+    if args.transformations:
+        # rotations
+        for rot in range(3):
+            tiff = numpy.rot90(tiff, 1, axes=(1, 2))
+            input_image = tiff[0]
+
+            for i in range(1, len(tiff)):
+                save_combination(args, input_image, tiff[i], args.prefix + file_path.split("/")[-1] + "_rotated_" + str(rot) + "_" + str(i))
+
+
+def pipeline(args):
+    if args.recursive:
+        for dir in tqdm.tqdm(os.listdir(args.input), desc="Directory: "):
+            for f in tqdm.tqdm(os.listdir(os.path.join(args.input, dir)), leave=True, desc="Files: "):
+                if os.path.isfile(os.path.join(args.input, dir, f)) and \
+                        args.file_suffix is not None and \
+                        f.endswith(args.file_suffix):
+                    handle_file(args, os.path.join(args.input, dir, f))
+    else:
+        for f in tqdm.tqdm(os.listdir(args.input), desc="Files: "):
+            if os.path.isfile(os.path.join(args.input, f)) and \
+                    args.file_suffix is not None and \
+                    f.endswith(args.file_suffix):
+                handle_file(args, os.path.join(args.input, f))
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    pipeline(args)
---
+++ Training.py
@@ -0,0 +1,120 @@
+import argparse
+import os
+import pickle
+import sys
+
+import lightning.pytorch.loggers
+import torch.utils.data
+import numpy.random
+import torch
+import lightning.pytorch as lp
+numpy.random.seed(1)
+
+class CycifSuperResolutionModel(lp.LightningModule):
+    def __init__(self, args):
+        pass
+
+    def training_step(self, batch):
+        x = self.model(batch)
+
+        return x
+
+    def configure_optimizers(self):
+        optim = torch.optim.Adam(self.parameters(), lr=1e-3)
+        return [optim], [torch.optim.lr_scheduler.ReduceLROnPlateau(optim, factor=0.1, patience=5, verbose=True, threshold=0.05)]
+
+
+class CycifDataModule(lp.LightningDataModule):
+    def __init__(self, args):
+        all_files = os.listdir(os.path.join(args.input, "input"))
+        numpy.random.shuffle(all_files)
+        train_f = args.frac_train * len(all_files)
+        rest = (1 - args.frac_train)/2
+        rest *= len(all_files)
+        self.base_path = args.input
+        self.files_train = all_files[0:train_f]
+        self.files_test = all_files[train_f:train_f+rest]
+        self.files_val = all_files[train_f+rest:]
+        self.batch_size = args.batch_size
+
+    def train_dataloader(self):
+        return CustomDataloader(self.base_path, self.files_train)
+
+    def test_dataloader(self):
+        return CustomDataloader(self.base_path, self.files_test)
+
+    def val_dataloader(self):
+        return CustomDataloader(self.base_path, self.files_val)
+
+
+class CustomDataloader(torch.utils.data.Dataset):
+    def __init__(self, base_path, files):
+        self.basepath = base_path
+        self.files = files
+
+    def __getitem__(self, item):
+        x, y = None, None
+
+        with open(os.path.join(self.basepath, "input", self.files[item]), "rb") as f:
+            x = pickle.load(f)
+
+        with open(os.path.join(self.basepath, "output", self.files[item]), "rb") as f:
+            y = pickle.load(f)
+
+        return x, y
+
+def set_callbacks(args):
+    output = list()
+
+    if args.is_test_run:
+        output.append(lp.callbacks.BatchSizeFinder(mode="binsearch"))
+        output.append(lp.callbacks.LearningRateFinder(min_lr=0.0000001, max_lr=0.9))
+    else:
+        output.append(lp.callbacks.ModelCheckpoint(dirpath=args.output, monitor="val/loss", filename="epoch-{epoch:02d}_{val/loss:.4f}"))
+        output.append(lp.callbacks.LearningRateMonitor(logging_interval="step"))
+
+    return output
+
+def pipeline(args):
+    trainer = None
+
+    if args.is_test_run:
+        trainer = lp.trainer.Trainer(callbacks=set_callbacks(args),
+                                 accelerator="gpu",
+                                 gpus=args.gpu_string,
+                                 distributed_backend="ddp2")
+    else:
+        trainer = lp.trainer.Trainer(logger=lp.loggers.TensorBoardLogger(args.tensorboard_path, name=args.title),
+                                 callbacks=set_callbacks(args),
+                                 accelerator="gpu",
+                                 gpus=args.gpu_string,
+                                 distributed_backend="ddp2")
+
+    model = CycifSuperResolutionModel(args)
+
+    data = CycifDataModule(args)
+
+    trainer.fit(model, train_dataloaders=data)
+
+
+def parse_args():
+    output = argparse.ArgumentParser()
+
+    output.add_argument("input", type=str, help="Base path of training data.")
+    output.add_argument("--tensorboard-path", type=str, help="Path to store the tensorboard data.", default="./tensorboard")
+    output.add_argument("--is-test-run", action="store_true", help="This run does not run training but hyper parameter setting before training.")
+    output.add_argument("--gpu-string", type=str, help="Comma separated string with availabble gpus eg 0,1", default="1")
+    output.add_argument("--frac-train", type=float, help="Fraction of dataset to use as training.", default=0.9)
+    output.add_argument("--batch-size", type=int, help="Batch size for training.", default=32)
+
+    output = output.parse_args(sys.argv[1:])
+
+    if not os.path.exists(args.tensorboard_path):
+        os.makedirs(args.tensorboard_path, exist_ok=True)
+
+    return output
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    pipeline(args)
